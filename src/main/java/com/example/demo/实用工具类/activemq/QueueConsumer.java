f2ebcc457afc997a83ba835ecae85feb3cf51aac
qj18780122553@163.com <qj18780122553>
2021-03-07 22:11:53 +0800

    [ADD]ok

--- src/main/java/com/example/demo/实用工具类/activemq/QueueConsumer.java
+++ src/main/java/com/example/demo/实用工具类/activemq/QueueConsumer.java
@@ -1,6 +1,5 @@
 package com.example.demo.实用工具类.activemq;
 
-import com.boot.patentBackstage.web.common.Xml;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -15,16 +14,12 @@ import java.io.File;
  */
 @Component
 public class QueueConsumer {
-    @Autowired
-    private Xml xml;
 
     @JmsListener(destination = "${spring.activemq.patentQueue}")
     public void patentMsgReceiver(TextMessage textMessage) throws JMSException {
-        xml.patentHandle(new File(textMessage.getText()));
     }
 
     @JmsListener(destination = "${spring.activemq.lawQueue}")
     public void lawMsgReceiver(TextMessage textMessage) throws JMSException {
-        xml.lawHandle(new File(textMessage.getText()));
     }
 }
--- src/main/java/com/example/demo/实用工具类/hadoop/UploadFile.java
+++ src/main/java/com/example/demo/实用工具类/hadoop/UploadFile.java
@@ -1,32 +1,32 @@
-package com.example.demo.实用工具类.hadoop;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-
-import java.io.FileSystem;
-import java.io.IOException;
-import java.net.URI;
-
-/**
-  * @Description TODO Hadoop上传文件
-  * @author: qinjie
-  * @date: 2020/1/17
- **/
-public class UploadFile {
-    public static void main(String args[]){
-        try {
-                Configuration conf = new Configuration();
-            conf.set("dfs.client.use.datanode.hostname", "true");//添加此配置信息即可
-                FileSystem fs = FileSystem.get(URI.create("hdfs://139.9.87.17:9000"), conf, "dr.who");
-
-                Path srcPath = new Path("F://11.txt");
-                Path dstPath = new Path("/");
-
-            fs.copyFromLocalFile(true, true, srcPath, dstPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package com.example.demo.实用工具类.hadoop;
+//
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.fs.Path;
+//
+//import java.io.FileSystem;
+//import java.io.IOException;
+//import java.net.URI;
+//
+///**
+//  * @Description TODO Hadoop上传文件
+//  * @author: qinjie
+//  * @date: 2020/1/17
+// **/
+//public class UploadFile {
+//    public static void main(String args[]){
+//        try {
+//                Configuration conf = new Configuration();
+//            conf.set("dfs.client.use.datanode.hostname", "true");//添加此配置信息即可
+//                FileSystem fs = FileSystem.get(URI.create("hdfs://139.9.87.17:9000"), conf, "dr.who");
+//
+//                Path srcPath = new Path("F://11.txt");
+//                Path dstPath = new Path("/");
+//
+//            fs.copyFromLocalFile(true, true, srcPath, dstPath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
